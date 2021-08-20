20d2234b3bd17fd658b26436351814579a8b9012
Tanmay000009 <tanmayvyas09@gmail.com>
2021-08-20 21:18:35 +0530

    Code refactored

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -11,6 +11,7 @@ export class Search extends Component {
         searchUsers: PropTypes.func.isRequired,
         clearUsers: PropTypes.func.isRequired,
         showClear: PropTypes.bool.isRequired,
+        setAlert: PropTypes.func.isRequired,
     };
 
     // event.taget.name as same function can be reused in case of multiple inputs
@@ -18,19 +19,25 @@ export class Search extends Component {
         
     onSubmit = (e) => {
         e.preventDefault();
-        this.props.searchUsers(this.state.text);
-        this.setState({text: ''});
+        if (this.state.text === '') {
+            this.props.setAlert('Please enter something','light');
+        } else {
+            this.props.searchUsers(this.state.text);
+            this.setState({text: ''});
+        }
     };
 
     render() {
+        const {showClear,clearUsers} = this.props;
+
         return (
             <div>
                 <form className="form" onSubmit={this.onSubmit}>
                     <input type="text" name="text" id="" placeholder="Search Users..." value={this.state.text} onChange={this.onChange} />
                     <input type="submit" value="Search" className="btn btn-dark btn-block" />
                 </form>
-                {this.props.showClear && 
-                <button className="btn btn-light btn-block" onClick={this.props.clearUsers}>Clear</button>
+                {showClear && 
+                <button className="btn btn-light btn-block" onClick={clearUsers}>Clear</button>
                 }
                 
             </div>
