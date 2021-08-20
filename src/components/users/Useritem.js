4e7a88b36561584e8f27a4bca682121d93a9bc5f
Tanmay000009 <tanmayvyas09@gmail.com>
2021-08-20 19:13:36 +0530

    Refactored code
    Class based components to function based

--- src/components/users/Useritem.js
+++ src/components/users/Useritem.js
@@ -1,15 +1,7 @@
-import React, { Component } from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
 
-class Useritem extends Component {
-    state = {
-        id: 'id',
-        login: 'mojombo',
-        avatar_url: 'https://avatars.githubusercontent.com/u/1?v=4',
-        html_url: 'https://github.com/mojombo'
-    }
-
-    render() {
-        const {login,avatar_url,html_url} = this.state;
+const  Useritem = ({user : {login,avatar_url,html_url}}) => {
         return (
             <div className="card text-center">
                 <img src={avatar_url} alt="" className='round-img' style={{width: '60px' }} />
@@ -21,7 +13,10 @@ class Useritem extends Component {
                </div>
             </div>
         )
-    }
+};
+
+Useritem.prototype = {
+    user: PropTypes.object.isRequired,
 }
 
 export default Useritem
