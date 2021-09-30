8528cef568dec761792f541b4ae35c1a4c944d3d
Tanmay000009 <tanmayvyas09@gmail.com>
2021-09-30 22:39:32 +0530

    refactor: remove redundant code

--- src/App.js
+++ src/App.js
@@ -12,18 +12,6 @@ import GithubState from "./context/github/GithubState";
 import AlertState from "./context/alert/AlertState";
 
 const App = () => {
-
-  // useEffect(  () => {
-  //   const fetchData = async () => {
-  //     setLoading(true);
-  //     const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
-  //     &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-  //     setUsers(res.data);
-  //     setLoading(false);
-  //   }
-  //   fetchData();
-  //   // eslint-disable-next-line 
-  // }, []);
   return (
     <GithubState>
       <AlertState>
